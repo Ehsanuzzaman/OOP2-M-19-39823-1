b60fa61d2fccdd6a171fa03f52cba6fcf024e372
Ehsanuzzaman <ehsanuzzaman113@gmail.com>
2021-02-08 08:50:35 +0600

    Task 1

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            Console.WriteLine("Print Even Number :");
+            n = 20;
+            for(i = 1; i<=20;i++)
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+           
+        }
+    }
+}
