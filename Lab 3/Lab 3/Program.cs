904fdf910ab86bd849aa4a762f20ebfb5a794f21
Ehsanuzzaman <ehsanuzzaman113@gmail.com>
2021-02-08 11:13:53 +0600

    Lab 3

---
+++ Lab 3/Lab 3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+   
+         class Student
+        {
+            private string name;
+            private string id;
+            private string department;
+            private float cgpa;
+
+            public void  ShowInfo()
+            {
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("ID: " + id);
+                Console.WriteLine("Department: " + department);
+                Console.WriteLine("CGPA: " + cgpa);
+            }
+
+            public void SetName(string name)
+            {
+                this.name = name;
+            }
+            public void SetID(string id)
+            {
+                this.id = id;
+            }
+            public void SetDepartment(string department)
+            {
+                this.department = department;
+            }
+            public void SetCGPA(float cgpa)
+            {
+                this.cgpa = cgpa;
+            }
+
+            public string getName()
+            {
+                return name;
+            }
+            public string getID()
+            {
+                return id;
+            }
+            public string getDepartment()
+            {
+                return department;
+            }
+            public float getCGPA()
+            {
+                return cgpa;
+            }
+        }
+
+        }
+    }
+}
