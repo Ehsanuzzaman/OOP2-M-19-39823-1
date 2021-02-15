4c2731a97386f61504e31ed4bb89ebe2d2d686af
Ehsanuzzaman <ehsanuzzaman113@gmail.com>
2021-02-15 12:54:32 +0600

    Lab_4 Task-4

---
+++ Lab_4 Task-4/Lab_4 Task-4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[] { 5,7,8,7,5 };
+            Console.WriteLine();
+
+            Console.WriteLine("Total number of duplicate elements  are:");
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = i + 1; j < arr1.Length; j++)
+                {
+                    if (arr1[i] == arr1[j])
+
+                        Console.WriteLine(arr1[j]);
+                }
+            }
+        }
+    }
+}
+    
+
---
+++ Lab_4 Task-5/Lab_4 Task-5/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, count = 0;
+            int[] arr5 = new int[100];
+            int i, j, k;
+            Console.Write("Printing all unique elements of the array:\n");
+            Console.Write("Input the number of the array :");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Input {0} elements into the array :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                arr5[i] = int.Parse(Console.ReadLine());
+            }
+            
+            Console.Write("The unique elements into the array are : \n");
+            for (i = 0; i < n; i++)
+            {
+                count = 0;
+                
+                for (j = 0; j < i - 1; j++)
+                {
+                    
+                    if (arr5[i] == arr5[j])
+                    {
+                        count++;
+                    }
+                }
+               
+                for (k = i + 1; k < n; k++)
+                {
+                   
+                    if (arr5[i] == arr5[k])
+                    {
+                        count++;
+                    }
+                   
+                    if (arr5[i] == arr5[i + 1])
+                    {
+                        i++;
+                    }
+                }
+               
+                if (count == 0)
+                {
+                    Console.Write("{0} ", arr5[i]);
+                }
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
+
+      
+    
+
