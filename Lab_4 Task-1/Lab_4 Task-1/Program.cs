5aaf09060a4b4ae3360be11b8609bbff4b9a9015
Ehsanuzzaman <ehsanuzzaman113@gmail.com>
2021-02-15 10:54:04 +0600

    Lab_4 Task-1

---
+++ Lab_4 Task-1/Lab_4 Task-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, i, j;
+            Console.Write("Sum of element in array:\n");
+
+
+            Console.Write("\nInput the number of elements of the array :");
+
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+
+            int[] arr = new int[100];
+
+            for (i = 1; i <= n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (j = arr[n]; j >= arr[1]; j--)
+            {
+                Console.Write(j);
+            }
+
+        }
+    }
+}
---
+++ Lab_4 Task-2/Lab_4 Task-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, i, sum = 0;
+
+            Console.Write("Sum of element in array:\n");
+
+
+            Console.Write("Input the number of elements of the array :");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+
+            int[] arr = new int[100];
+
+            for (i = 1; i <= n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                sum = sum + arr[i];
+            }
+            Console.Write(sum);
+
+        }
+    }
+}
+
+
